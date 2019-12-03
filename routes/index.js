907cfea12f5720d79c950727b2747b06bb2a7102
Manav Bokinala <manav.bokinala@gmail.com>
2019-12-02 22:07:38 -0800

    added printout of data received

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   res.status(200).send('received data successfully');
+  console.log(res.data)
 });
 
 module.exports = router;
